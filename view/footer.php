61fdaacfcbb075f1d3b43018d8d92ace8e076d68
gmillican <geejayemm@gmail.com>
2017-04-21 11:18:10 -0400

    Add files via upload
    initial upload

---
+++ view/footer.php
@@ -0,0 +1,8 @@
+<html>
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> CMSY 203 Bowling League
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" type="text/css"
+          href="main.css">   
+<!-- the head section -->
+<head>
+    <title>CMSY 203 Bowling League</title>
+</head>
+
+<!-- the body section -->
+<body>
+<header>
+<h1>CMSY 203 Bowling League</h1>
+    <!-- <p>
+        <img src="./images/bowling.png"
+               alt="Image: bowling.png"
+               height="200"/>
+    </p> -->
+</header>
+</html>
