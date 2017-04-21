4ffa638e950262b937997134db6deb3886c1fc76
gmillican <geejayemm@gmail.com>
2017-04-21 11:15:30 -0400

    Add files via upload
    Initial upload. This is a modified version of the Chapter 5 code from class.

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <link rel="stylesheet" type="text/css" href="main.css"> 
+<?php include 'view/header.php'; ?>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        
+        <li>
+            <a href="bowling_player">Player Information</a>
+        </li>
+        <li>
+            <a href="bowling_team">Team Information</a>
+        </li>
+        <li>
+            <a href="bowling_league">League Information</a>
+        </li>
+        <li>
+            <a href="bowling_admin">Bowling Managers</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
+</html>
