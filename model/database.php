f638f72230cbcf969d3fca2ee5e867efe89af344
gmillican <geejayemm@gmail.com>
2017-04-24 17:02:26 -0400

    Replaces the db access scripts
    THis will use the bowling database, so all the previously existing data
    access will fail.

--- model/database.php
+++ model/database.php
@@ -1,13 +1,14 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+
+	$dsn = 'mysql:host=localhost;dbname=bowling';
+    $username = 'demo';
+    $password = 'demo';
 
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
-        include('../errors/database_error.php');
+        include('database_error.php');
         exit();
     }
 ?>
